3744a67d0234db4117afc7061e24be851535a786
Lilian Hiault <lilian.hiault@laposte.net>
2019-12-06 12:28:09 +0100

    Fini tp1ex1.pl

--- tp1ex1.pl
+++ tp1ex1.pl
@@ -1,30 +1,35 @@
 /* --- Faits --- */
 
 % Personnes
-personne(01, Glorfindel, 1.90, Blond, 5000)
-personne(02, Sauron, 2.78, Brun, 10000)
-personne(03, Arwen, 1.70, Brun, 2901)
-personne(04, Aragorn, 1.98, Brun, 210)
-personne(06, Eowyn, 1.80, Blond, 30)
-personne(07, Faramir, 1.80, Chatain, 120)
+personne(01, glorfindel, 190, blond, 5000).
+personne(02, sauron, 278, brun, 10000).
+personne(03, arwen, 170, brun, 2901).
+personne(04, aragorn, 198, brun, 210).
+personne(06, eowyn, 180, blond, 30).
+personne(07, faramir, 180, chatain, 120).
 
 % Goûts
-gout(01, Metal, Fantastique, Escrime)
-gout(02, Metal, Horreur, Escrime)
-gout(03, Classique, Historique, Course)
-gout(04, Classique, Historique, Course)
-gout(05, Jazz, ScienceFiction, Equitation)
-gout(07, Rock, ScienceFiction, Escalade)
+gout(01, metal, fantastique, escrime).
+gout(02, metal, horreur, escrime).
+gout(03, classique, historique, course).
+gout(04, classique, historique, course).
+gout(05, jazz, scienceFiction, equitation).
+gout(07, rock, scienceFiction, escalade).
 
 % Recherche
-recherche(01, 2.78, Brun, 10000)
-recherche(02, 1.90, Blond, 5000)
-recherche(03, 1.98, Brun, 210)
-recherche(04, 1.70, Brun, 2901)
-recherche(06, 1.80, Chatain, 120)
-recherche(07, 1.80, Blond, 30)
+recherche(01, 278, brun, 10000).
+recherche(02, 190, blond, 5000).
+recherche(03, 198, brun, 210).
+recherche(04, 170, brun, 2901).
+recherche(06, 180, chatain, 120).
+recherche(07, 180, blond, 30).
 
 /* --- Règles --- */
 
-convient-physiquement(X, Y)
-ont-memes-gouts(X, Y) :- gout(personne(I, X, T, C, A), M, L, S), gout(personne(I, Y, T, C, A), M, L, S)
+convient-physiquement(X, Y) :- personne(X, N, T, C, A), recherche(Y, T, C, A).
+ont-memes-gouts(X, Y) :- gout(X, M, L, S), gout(Y, M, L, S).
+couples(X, Y) :- ont-memes-gouts(X, Y), convient-physiquement(X, Y), convient-physiquement(Y, X).
+
+/* --- Questions --- */
+
+%?- couples(X, Y).
